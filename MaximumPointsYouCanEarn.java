d04a1739c9de4805b0c406400cfcef8119f611b1
2004shweta <156895477+2004shweta@users.noreply.github.com>
2024-11-08 00:52:38 +0530

    Create MaximumPointsYouCanEarn.java

---
+++ MaximumPointsYouCanEarn.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int lsum=0,rsum=0,maxsum=0;
+        int n=cardPoints.length;
+
+        for(int i=0;i<k;i++){
+           lsum+=cardPoints[i];
+          
+        }
+        maxsum=lsum;
+        int r=n-1;
+        for(int i =k-1;i>=0;i--){
+            lsum=lsum-cardPoints[i];
+            rsum=rsum+cardPoints[r];
+            r--;
+            maxsum=Math.max(maxsum,rsum+lsum);
+        }
+          
+     
+        return maxsum;
+    }
+   
+}
